cbbe260b88c34d4577e0862282afb9af45c98e65
wang.haoyu <228233464@qq.com>
2021-07-09 14:59:03 +0800

    feat(transform): ast

--- parse.js
+++ parse.js
@@ -3,10 +3,10 @@ const SYMBOL_REG = /\(|\)|;|,/;
 const NUMBER_REG = /\d/;
 const NAME_REG = /[a-zA-Z]/;
 
+// transform token
 function tokenizer(input) {
   let currentIndex = 0;
   const tokens = [];
-  debugger;
   while (currentIndex < input.length) {
     let char = input[currentIndex];
 
@@ -55,4 +55,59 @@ function tokenizer(input) {
   return tokens;
 }
 
-console.log(tokenizer('(add 2 (subtract 4 2))'));
+// get ast
+function parse(tokens) {
+  const ast = {
+    type: 'program',
+    body: [],
+  };
+
+  let currentIndex = 0;
+
+  function walk() {
+    // 首先判断是否是数字 暂时不处理变量
+    let token = tokens[currentIndex];
+
+    if (token.type === 'number') {
+      currentIndex++;
+      return {
+        value: token.value,
+        type: 'NumberLiteral',
+      };
+    }
+
+    // lisp 函数
+    if (token.type === 'symbol' && token.value === '(') {
+      token = tokens[++currentIndex];
+
+      const node = {
+        type: 'CallExpression',
+        name: token.value,
+        params: [],
+      };
+
+      token = tokens[++currentIndex];
+      debugger;
+
+      while (
+        token.type !== 'symbol' ||
+        (token.type === 'symbol' && token.value !== ')')
+      ) {
+        node.params.push(walk());
+        token = tokens[currentIndex];
+      }
+      // 不要忘记跳过 右括号
+      currentIndex++;
+      return node;
+    }
+  }
+
+  while (currentIndex < tokens.length) {
+    ast.body.push(walk());
+  }
+
+  return ast;
+}
+
+const tokens = tokenizer('(add 2 (subtract 4 2))');
+console.log(JSON.stringify(parse(tokens)), 'ast');
